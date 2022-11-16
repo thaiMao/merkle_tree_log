0fc52bbdb8a31a5f2e743d13eb2b43b7b3552e3b
thai <maot.tm@gmail.com>
2022-11-16 12:14:17 +0000

    declare treehash methods

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use std::sync::{Arc, Mutex};
+use std::thread;
 
 #[derive(Clone, Debug)]
 struct Stack {}
@@ -21,6 +22,38 @@ struct Node {}
 #[derive(Clone, Debug)]
 struct TreeHash {
     stack: Arc<Mutex<Stack>>,
+    node: Option<Node>,
+    index: usize,
+}
+
+impl TreeHash {
+    fn new(stack: Arc<Mutex<Stack>>) -> Self {
+        Self {
+            stack,
+            index: 0,
+            node: None,
+        }
+    }
+
+    fn initialize(&mut self, index: usize) {
+        self.index = 0;
+        self.node = None;
+    }
+
+    fn lowest_tail_node_height() -> usize {
+        todo!();
+    }
+
+    /// Executes the treehash alogirthm once.
+    /// After the last call the stack contains one node, the desired inner node on height h.
+    fn update(&mut self) {
+        let stack = Arc::clone(&self.stack);
+        thread::spawn(move || {
+            let stack = stack.lock();
+        });
+
+        todo!();
+    }
 }
 
 #[derive(Clone, Debug)]
